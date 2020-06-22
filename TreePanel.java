2b733a0f0e4e3ec3063eaadd96c3b54c58941bce
Evan Suslovich <evan.suslovich@lincolnps.org>
2020-06-22 11:25:17 -0400

    Added 'addBush(TreeIcon bush)' and 'bushes' linkedList.

--- TreePanel.java
+++ TreePanel.java
@@ -8,17 +8,26 @@ public class TreePanel extends JPanel{
     private static final long serialVersionUID = 1; 
 
     private List<TreeIcon> trees = new LinkedList<TreeIcon>(); 
+    private List<TreeIcon> bushes = new LinkedList<TreeIcon>(); 
 
-    public void addTree(TreeIcon tree)
-    {
+    public void addTree(TreeIcon tree){
         trees.add(tree); 
         this.repaint(); 
+    }
+    public void addBush(TreeIcon bush){
+        bushes.add(bush);
+        this.repaint();
     }
     @Override
     public void paint(Graphics g){
         for(TreeIcon t : trees){
-            t.draw(g); 
+            t.drawTree(g); 
+        }
+        for(TreeIcon b : bushes){
+            b.drawBush(g);
         }
     }
+    
+    
 
 }
