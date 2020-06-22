cc40aba36bc799fd9f66eab810b29e8e912b24ae
Evan Suslovich <evan.suslovich@lincolnps.org>
2020-06-22 11:26:58 -0400

    Changed the name from menu.java to SidePanel.java . Also added the code for the bush

---
+++ SidePanel.java
@@ -0,0 +1,85 @@
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+
+
+import java.awt.*; 
+import java.awt.event.*;
+
+
+public class SidePanel extends JPanel{
+    private static final long serialVersionUID = 1; 
+
+    //private TreeClickListener click; 
+    //private MouseEvent e; 
+
+    
+
+    JButton treeButton = new JButton("Tree");
+    JButton bushButton = new JButton("Bush");
+
+    private ImageIcon treeImage; 
+    private JLabel treeLabel; 
+    JPanel treePanel = new JPanel();
+
+
+    private ImageIcon bushImage; 
+    private JLabel bushLabel; 
+    JPanel bushPanel = new JPanel(); 
+
+    String select; 
+
+    
+    
+
+    public SidePanel(){
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS)); 
+        setBorder(new LineBorder(Color.BLACK,3)); 
+
+
+        //////////////////////////////// tree image 
+        treeImage = new ImageIcon(getClass().getResource("treeImage.png")); 
+        treeLabel = new JLabel(treeImage); 
+
+        treePanel.setLayout(new FlowLayout()); 
+        treePanel.add(treeButton); 
+        treePanel.add(treeLabel); 
+        add(treePanel); 
+        
+
+        //////////////////////////////// bush image
+
+        bushImage = new ImageIcon(getClass().getResource("bushImage.png")); 
+        bushLabel = new JLabel(bushImage);
+
+        bushPanel.setLayout(new FlowLayout()); 
+        bushPanel.add(bushButton); 
+        bushPanel.add(bushLabel); 
+        add(bushPanel); 
+
+        actionListener(); 
+
+    }
+
+    public void actionListener(){
+        treeButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                select = "tree"; 
+                
+            }
+        }); 
+
+        bushButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                select = "bush"; 
+            }
+        });
+    }
+
+    public String getSelect(){
+        return select; 
+    }
+
+
+    
+
+}
