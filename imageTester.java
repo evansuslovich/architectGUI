9d5133d12acda1e79a04e21c5e8d9dfd7082c51b
Evan Suslovich <evan.suslovich@lincolnps.org>
2020-06-22 13:39:40 -0400

    Church does not work because of the background. Next stop house

--- imageTester.java
+++ imageTester.java
@@ -2,6 +2,28 @@
 import javax.swing.*; 
 
 public class imageTester extends JFrame{
+    private static final long serialVersionUID = 1; 
+
+    JPanel panel = new JPanel(); 
+    private  ImageIcon image; 
+    private  JLabel imageLabel;
+
+
+    public imageTester(){
+        setSize(800,800);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("Image Tester"); 
+
+        image = new ImageIcon(getClass().getResource("churchFullImage.png")); 
+        imageLabel = new JLabel(image); 
+
+        panel.add(imageLabel); 
+        
+
+
+        add(panel); 
+        setVisible(true); 
+    }
 
     public static void main(String[] args){
         new imageTester(); 
